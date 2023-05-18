edc7dc728dab4007f26bee401a59be8fb23d132c
Cyril Cheza <cyrilcheza@gmail.com>
2023-05-18 20:13:34 +0100

    adding tests for step 1

---
+++ Courier Calculator/CourierTests/ParcelShippingTests.cs
@@ -0,0 +1,66 @@
+﻿using CourierLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CourierTests
+{
+
+    public class ParcelShippingTests
+    {
+        [TestMethod]
+        public void CalculateTotalCost_SmallParcel_CorrectCost()
+        {
+            // Arrange
+            Parcel parcel = new Parcel(1, 1, 1); 
+
+            // Act
+            double totalCost = ShippingService.CalculateTotalCost(parcel);
+
+            // Assert
+            Assert.AreEqual(3.0, totalCost);
+        }
+
+        [TestMethod]
+        public void CalculateTotalCost_MediumParcel_CorrectCost()
+        {
+            // Arrange
+            Parcel parcel = new Parcel(30, 30, 30);
+
+            // Act
+            double totalCost = ShippingService.CalculateTotalCost(parcel);
+
+            // Assert
+            Assert.AreEqual(8.0, totalCost);
+        }
+
+        [TestMethod]
+        public void CalculateTotalCost_LargeParcel_CorrectCost()
+        {
+            // Arrange
+            Parcel parcel = new Parcel(80, 80, 80);
+
+            // Act
+            double totalCost = ShippingService.CalculateTotalCost(parcel);
+
+            // Assert
+            Assert.AreEqual(15.0, totalCost);
+        }
+
+        [TestMethod]
+        public void CalculateTotalCost_XLParcel_CorrectCost()
+        {
+            // Arrange
+            Parcel parcel = new Parcel(120, 120, 120); 
+
+            // Act
+            double totalCost = ShippingService.CalculateTotalCost(parcel);
+
+            // Assert
+            Assert.AreEqual(25.0, totalCost);
+        }
+    }
+
+}
+
+
+
---
+++ Courier Calculator/CourierTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CourierTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CourierTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30977e4f-e6a0-4f0e-87d6-fd6db1d1ed09")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
